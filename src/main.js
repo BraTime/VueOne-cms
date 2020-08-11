ee2d07726d30d5f0b34d61af6a4378c502bafe68
BraTime <1125428988BraTime@users.noreply.github.com>
2020-08-11 23:29:09 +0800

    my project

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@
 
 // 入口文件
 import Vue from 'vue';
+// 导入路由包
+import VueRouter from "vue-router"
+// 安装路由
+Vue.use(VueRouter)
+
+
 
 // 导入 mui的样式
 import './lib/mui/css/mui.min.css';
+// 导入 mui 中的扩展图标的样式 
+// 注意: 将他对应的扩展字体库mui-icons-extra.ttf拷贝到项目fonts中
+import './lib/mui/css/icons-extra.css'
 
 
 
@@ -14,13 +23,13 @@ import { Header } from 'mint-ui';
 Vue.component(Header.name , Header)
 
 
-
-
 // 导入app 组件
 import app from './App.vue'
-
+// 导入自己的 路由模块 router.js
+import router from './router';
 
 var vm = new Vue({
   el: "#app",
-  render: c => c(app) // 渲染 app 组件
+  render: c => c(app), // 渲染 app 组件
+  router, // 挂载路由 router
 })
--- src/router.js
+++ src/router.js
@@ -1,10 +1,21 @@
 import VueRouter from 'vue-router'
 
+// 导入 对应的路由组件
+import HomeContainer from "./components/tabbar/HomeContainer.vue"
+import MemberContainer from "./components/tabbar/MemberContainer.vue"
+import ShopcarContainer from "./components/tabbar/ShopcarContainer.vue"
+import SearchContainer from "./components/tabbar/SearchContainer.vue"
+
 // 3. 创建路由对象
 var router = new VueRouter({
-  routes: [
-    
-  ]
+  routes: [ // 配置路由规则的
+    { path: '/home' , component: HomeContainer},
+    { path: '/member' , component: MemberContainer},
+    { path: '/shopcar' , component: ShopcarContainer},
+    { path: '/search' , component: SearchContainer}
+  ],
+  // 覆盖默认的路由高亮类(当前元素特有的类) 默认类名：router-link-active
+  linkActiveClass: 'mui-active' 
 })
 
 // 把路由对象暴露出去
